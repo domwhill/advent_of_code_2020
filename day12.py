e319aea65ba99fc56e313044bf0a9390afdf8718
domwhill <dominicwhill@gmail.com>
2020-12-13 21:35:15 +0000

    advent of code day 12 - part 1

---
+++ day12.py
@@ -0,0 +1,94 @@
+"""Advent of code day 12
+
+
+Action N means to move north by the given value.
+Action S means to move south by the given value.
+Action E means to move east by the given value.
+Action W means to move west by the given value.
+Action L means to turn left the given number of degrees.
+Action R means to turn right the given number of degrees.
+Action F means to move forward by the given value in the direction the ship is currently facing.
+"""
+import re
+import numpy as np
+# star facing east
+
+def get_manhatten_distance(position):
+    return np.sum(np.abs(position))
+
+
+def deg_to_radians(deg):
+    return (deg/360.0)*(2.0*np.pi)
+
+
+def get_orientation_from_angle(deg):
+    rad = deg_to_radians(deg)
+    return np.array([np.cos(rad), np.sin(rad)])
+
+
+def update_ship_orientation(ship_orientation, direction, step):
+    position, angle = ship_orientation
+    if direction in ("L", "R"):
+        angle = update_angle(angle, direction, step)
+    else:
+        position = update_position(angle, direction, position, step)
+    return (position, angle)
+
+def update_position(angle, direction, position, step):
+    direction_lookup = \
+        {"N": np.array([0.0, 1.0]),
+         "S": np.array([0.0, -1.0]),
+         "E": np.array([1.0, 0.0]),
+         "W": np.array([-1.0, 0.0]),
+         "F": get_orientation_from_angle(angle)}
+    position += direction_lookup[direction] * step
+    return position
+
+def update_angle(angle, direction, step):
+    angle_lookup = {"L": 1, "R": -1}
+    angle += angle_lookup[direction] * step
+    return angle
+
+
+def read_file(file_name):
+    f = open(file_name, "r")
+    data = f.readlines()
+    f.close()
+    return data
+
+def main():
+    file_name = "day12_test_input.txt"
+    file_name = "day12_input.txt"
+
+    data = read_file(file_name)
+    position, angle = np.array([0.0, 0.0]), 0.0
+    ship_orientation = (position, angle)
+    for line in data:
+        mline = re.search("^([A-Z])([0-9]+)", line)
+        direction, step = mline.groups()[0], int(mline.groups()[1])
+        ship_orientation = update_ship_orientation(ship_orientation, direction, step)
+
+        print(f"ship_orientation = {ship_orientation}, instruction = {direction}, {step}")
+
+    print("final position = ", position, get_manhatten_distance(position))
+
+def main2():
+    file_name = "day12_test_input.txt"
+    file_name = "day12_input.txt"
+
+    data = read_file(file_name)
+    position, angle = np.array([0.0, 0.0]), 0.0
+    waypoint_position, waypoint_angle = np.array([10.0, 1.0]), 0.0
+
+    ship_orientation = (position, angle)
+    for line in data:
+        mline = re.search("^([A-Z])([0-9]+)", line)
+        direction, step = mline.groups()[0], int(mline.groups()[1])
+        ship_orientation = update_ship_orientation(ship_orientation, direction, step)
+
+        print(f"ship_orientation = {ship_orientation}, instruction = {direction}, {step}")
+
+    print("final position = ", position, get_manhatten_distance(position))
+
+if __name__ == "__main__":
+    main()
